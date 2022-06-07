91eaa90270fa621d16df59073ccfb88d1bfd63b5
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-06-07 23:01:19 +0900

    feat: add symlink script

--- script/link.sh
+++ script/link.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ln -s ./config/zsh/.zshrc ~
+find ./config/zsh -type f -print | xargs -I{} ln -s {} ~
+ln -s ./config/p10k/.p10k.zsh ~
+
+mkdir ~/.config
+ln -s ./config/navi ~/.config
+ln -s ./config/zeno ~/.config
