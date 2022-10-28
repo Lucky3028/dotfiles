5d82dd5af8d1aaa13d6ea523d4a8bac2e0d1cde6
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-28 21:34:58 +0900

    feat: add link script

--- script/link.sh
+++ script/link.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-find ./config/zsh -type f -print | xargs -I{} ln -s {} ~
-ln -s ./config/p10k/.p10k.zsh ~
+# Move current dir to repository root
+# cf. 
+cd $(dirname $0)/../
+find $(pwd)/config/zsh -type f -print | xargs -I{} ln -s {} ~/
+ln -s $(pwd)/config/p10k/.p10k.zsh ~/
+ln -s $(pwd)/config/vim/.vimrc ~/
+ln -s $(pwd)/config/git/.gitconfig ~/
 
 mkdir ~/.config
-ln -s ./config/navi ~/.config
-ln -s ./config/zeno ~/.config
+ln -s $(pwd)/config/navi ~/.config/
+ln -s $(pwd)/config/zeno ~/.config/
+ln -s $(pwd)/config/gh-cli ~/.config/
