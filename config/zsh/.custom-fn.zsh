39d0cd21bc73069d75070f7330feaa0797a8b267
Lucky3028 <me@mail.cap-l.cc>
2024-02-06 23:15:15 +0900

    docs: add docs for ga fun

--- config/zsh/.custom-fn.zsh
+++ config/zsh/.custom-fn.zsh
@@ -1,3 +1,5 @@
+# git addするファイルをfzfを用いて選択する
+# See: https://dev.classmethod.jp/articles/fzf-original-app-for-git-add/
 function ga() {
   local selected
   selected=$(unbuffer git status -s | fzf -m --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
