b6603162bc53612f4a4bd1d824618c37a250f643
Lucky3028 <me@mail.cap-l.cc>
2024-02-07 00:21:14 +0900

    feat: add git add with fzf fun

---
+++ config/zsh/.custom-fn.zsh
@@ -0,0 +1,9 @@
+function ga() {
+  local selected
+  selected=$(unbuffer git status -s | fzf -m --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
+  if [ -n "$selected" ]; then
+    selected=$(tr '\n' ' ' <<<"$selected")
+    git add "$selected"
+    echo "Completed: git add $selected"
+  fi
+}
