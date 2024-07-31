45d97ce1a046e9626177cb1e3a528be6178a2ea6
Lucky3028 <yubi@clov3r.cc>
2024-07-31 23:32:08 +0900

    refactor: move some files and dirs to .config dir

--- src/run_onchange_03_install-proto.sh
+++ src/run_onchange_03_install-proto.sh
@@ -5,7 +5,5 @@ set -euxo pipefail
 curl -fsSL https://moonrepo.dev/install/proto.sh \
   | bash -s -- --yes --no-modify-profile --no-modify-path
 
-set +x && source ~/.zshenv && set -x
-
-proto completions --shell zsh > $ZSH_COMP/_proto
+proto completions --shell zsh > $ZDOTDIR/completions/_proto
 
