fd7c82177f368ef5f55a88f0fd82862fa8db4e83
Lucky <yubi@clov3r.cc>
2024-08-30 11:49:18 +0900

    fix: add command to use proto plugin for terraform

--- src/run_once_05_install-terraform.sh
+++ src/run_once_05_install-terraform.sh
@@ -2,5 +2,6 @@
 
 set -euxp pipefail
 
+$PROTO_HOME/bin/proto plugin add terraform 'https://raw.githubusercontent.com/theomessin/proto-toml-plugins/master/terraform.toml'
 $PROTO_HOME/bin/proto install terraform
 
