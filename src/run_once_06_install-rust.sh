3be947879ea71a30181053f7e40377c490ba3a11
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-09-27 12:47:35 +0900

    feat: use Rust with mise

--- src/run_once_06_install-rust.sh
+++ src/run_once_06_install-rust.sh
@@ -2,12 +2,9 @@
 
 set -euxo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
-  sh -s -- --no-modify-path --default-toolchain stable --profile default -y
-
 # Add completions
 ## rustup
-$CARGO_HOME/bin/rustup completions zsh > $ZDOTDIR/completions/_rustup
+$(which rustup) completions zsh > $ZDOTDIR/completions/_rustup
 ## cargo
-$CARGO_HOME/bin/rustup completions zsh cargo > $ZDOTDIR/completions/_cargo
+$(which rustup) completions zsh cargo > $ZDOTDIR/completions/_cargo
 
