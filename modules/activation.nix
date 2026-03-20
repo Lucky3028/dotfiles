04516eb11346d77dc7776c7e1feb03affaa21aba
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2026-03-20 22:33:21 +0900

    feat: ensure mise completions directory exists on activation

--- modules/activation.nix
+++ modules/activation.nix
@@ -2,6 +2,7 @@
   home.activation.createDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p $HOME/.config/zsh/completions
     mkdir -p $HOME/bin
+    mkdir -p $HOME/.local/share/mise/completions
     mkdir -p $HOME/.local/state/zsh
     install -d -m 700 $HOME/.ssh
   '';
